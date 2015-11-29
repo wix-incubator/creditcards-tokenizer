293832478c6eaacf0cbbb9cc9f08bdb40a0ec96c
Danny Leshem <dleshem@gmail.com>
2015-11-29 12:22:38 +0200

    Promise -> Q

--- src/CommonProtocolClient.js
+++ src/CommonProtocolClient.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import Q from 'q'
+
 export class CommonProtocolClient {
 	constructor({XMLHttpRequest, endpointUrl, timeout}) {
 		this._XMLHttpRequest = XMLHttpRequest
@@ -7,40 +9,42 @@ export class CommonProtocolClient {
 		this._timeout = timeout || 0
 	}
 	doRequest(resource, request) {
-		return new Promise((resolve, reject) => {
-			let xhr = new this._XMLHttpRequest()
-			xhr.ontimeout = () => {
-				reject({
-					code: 'timeout',
-					description: 'request timed out'
-				})
-			}
-			xhr.onerror = () => {
-				reject({
-					code: 'network_down',
-					description: 'network is down'
-				})
-			}
-			xhr.onload = () => {
-				try {
-					let response = JSON.parse(xhr.response)
-					if (response.error) {
-						reject(response.error)
-					} else {
-						resolve(response.value)
-					}
-				} catch (e) {
-					reject({
-						code: 'protocol',
-						description: 'unexpected response format'
-					})
+		let deferred = Q.defer()
+		
+		let xhr = new this._XMLHttpRequest()
+		xhr.ontimeout = () => {
+			deferred.reject({
+				code: 'timeout',
+				description: 'request timed out'
+			})
+		}
+		xhr.onerror = () => {
+			deferred.reject({
+				code: 'network_down',
+				description: 'network is down'
+			})
+		}
+		xhr.onload = () => {
+			try {
+				let response = JSON.parse(xhr.response)
+				if (response.error) {
+					deferred.reject(response.error)
+				} else {
+					deferred.resolve(response.value)
 				}
+			} catch (e) {
+				deferred.reject({
+					code: 'protocol',
+					description: 'unexpected response format'
+				})
 			}
-			
-			xhr.open('POST', `${this._endpointUrl}${resource}`, true)
-			xhr.timeout = this._timeout
-			xhr.setRequestHeader('Content-Type', 'application/json')
-			xhr.send(JSON.stringify(request))
-		})
+		}
+		
+		xhr.open('POST', `${this._endpointUrl}${resource}`, true)
+		xhr.timeout = this._timeout
+		xhr.setRequestHeader('Content-Type', 'application/json')
+		xhr.send(JSON.stringify(request))
+		
+		return deferred.promise
 	}
 }
